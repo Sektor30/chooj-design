f1751bb48a27e0678c999cc7b1399a731bad30e8
Sektor30 <sektor9130@gmail.com>
2024-09-09 11:40:47 -0400

    6v 0 09

---
+++ js/bd-curso.js
@@ -0,0 +1,224 @@
+
+    let modulos= [
+      {
+        
+        nombre: "Guiones",
+        
+        videos: [
+          { id: 1,
+            descripcion: "",
+            autor:"",
+            titulo: "Introducción al Guion Cinematográfico",
+            enlace: "https://www.youtube.com/watch?v=G46Hn4sOOWQ"
+          },
+          {id: 2,
+            descripcion: "",
+            autor:"",
+            titulo: "Estructura de un Guion",
+            enlace: "https://www.youtube.com/watch?v=NN0J5GVk-tU"
+          },
+          {id: 3,
+            descripcion: "",
+            autor:"",
+            titulo: "Desarrollo de Personajes",
+            enlace: "https://www.youtube.com/watch?v=fH8K7J9ZRHE"
+          },
+          {id: 4,
+            descripcion: "",
+            autor:"",
+            titulo: "Diálogos Efectivos",
+            enlace: "https://www.youtube.com/watch?v=wC_TTfzSgE4"
+          },
+          {id: 5,
+            descripcion: "",
+            autor:"",
+            titulo: "El Viaje del Héroe en el Guion",
+            enlace: "https://www.youtube.com/watch?v=_r7GBqBrJt8"
+          },
+          {id: 6,
+            descripcion: "",
+            autor:"",
+            titulo: "Creación de Mundos y Ambientación",
+            enlace: "https://www.youtube.com/watch?v=3grqfjY8SWc"
+          },
+          {id: 7,
+            descripcion: "",
+            autor:"",
+            titulo: "Cómo Escribir una Escena",
+            enlace: "https://www.youtube.com/watch?v=8vD6a4q4Z4Y"
+          },
+          {id: 8,
+            descripcion: "",
+            autor:"",
+            titulo: "Errores Comunes en el Guion",
+            enlace: "https://www.youtube.com/watch?v=IfaFTGPR9Jk"
+          }
+        ]
+      },
+      {
+        nombre: Edición,
+        videos: [
+          {id: 9,
+            descripcion: "",
+            autor:"",
+            titulo: "Fundamentos de la Edición de Video",
+            enlace: "https://www.youtube.com/watch?v=KFTk2VtGZ4E"
+          },
+          {id: 10,
+            descripcion: "",
+            autor:"",
+            titulo: "Uso de Adobe Premiere Pro",
+            enlace: "https://www.youtube.com/watch?v=n9-SUkXBw5U"
+          },
+          {id: 11,
+            descripcion: "",
+            autor:"",
+            titulo: "Técnicas de Montaje",
+            enlace: "https://www.youtube.com/watch?v=x1Us-JcmFZ0"
+          },
+          {id: 12,
+            descripcion: "",
+            autor:"",
+            titulo: "Corrección de Color Básica",
+            enlace: "https://www.youtube.com/watch?v=6FgWw27SUpg"
+          },
+          {id: 13,
+            descripcion: "",
+            autor:"",
+            titulo: "Efectos y Transiciones",
+            enlace: "https://www.youtube.com/watch?v=3WXy3Y_zMJU"
+          },
+          {id: 14,
+            descripcion: "",
+            autor:"",
+            titulo: "Edición de Audio en Video",
+            enlace: "https://www.youtube.com/watch?v=Ki7hY_yMGL4"
+          },
+          {id: 15,
+            descripcion: "",
+            autor:"",
+            titulo: "Edición en Final Cut Pro",
+            enlace: "https://www.youtube.com/watch?v=pLXgtpE89sE"
+          },
+          {id: 16,
+            descripcion: "",
+            autor:"",
+            titulo: "Errores Comunes en Edición",
+            enlace: "https://www.youtube.com/watch?v=56F-T_jvYRE"
+          }
+        ]
+      },
+      {
+        nombre: "Producción",
+        videos: [
+          {id: 17,
+            descripcion: "",
+            autor:"",
+            titulo: "Introducción a la Producción Cinematográfica",
+            enlace: "https://www.youtube.com/watch?v=3MkROzDFkGI"
+          },
+          {id: 18,
+            descripcion: "",
+            autor:"",
+            titulo: "Planificación de Producción",
+            enlace: "https://www.youtube.com/watch?v=6p4_hfAiV4A"
+          },
+          {id: 19,
+            descripcion: "",
+            autor:"",
+            titulo: "Presupuesto y Financiación de Proyectos",
+            enlace: "https://www.youtube.com/watch?v=WzFzSt2a9kE"
+          },
+          {id: 20,
+            descripcion: "",
+            autor:"",
+            titulo: "Logística en el Rodaje",
+            enlace: "https://www.youtube.com/watch?v=RQ7NwB5KxHo"
+          },
+          {id: 21,
+            descripcion: "",
+            autor:"",
+            titulo: "Dirección de Producción",
+            enlace: "https://www.youtube.com/watch?v=dX7F1Vt6T2Y"
+          },
+          {id: 22,
+            descripcion: "",
+            autor:"",
+            titulo: "Manejo de Recursos y Personal",
+            enlace: "https://www.youtube.com/watch?v=n3dLVR3vV8I"
+          },
+          {id: 23,
+            descripcion: "",
+            autor:"",
+            titulo: "Documentación y Contratos",
+            enlace: "https://www.youtube.com/watch?v=XFBc0IJQsyQ"
+          },
+          {id: 24,
+            descripcion: "",
+            autor:"",
+            titulo: "Errores en la Producción",
+            enlace: "https://www.youtube.com/watch?v=ZogcTleA3P8"
+          }
+        ]
+      },
+      {
+        nombre: "Géneros del Cine",
+        "videos": [
+          {id: 25,
+            descripcion: "",
+            autor:"",
+            titulo: "Historia y Evolución del Cine",
+            enlace: "https://www.youtube.com/watch?v=6rX8cf-Mw0Q"
+          },
+          {
+            id: 26,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine de Acción",
+            enlace: "https://www.youtube.com/watch?v=wLM5N0I8-L4"
+          },
+          {
+            id: 27,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine de Terror",
+            enlace: "https://www.youtube.com/watch?v=b7OX0TkP3G4"
+          },
+          {
+            id: 28,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine de Ciencia Ficción",
+            enlace: "https://www.youtube.com/watch?v=J9wICZYuRjk"
+          },
+          {
+            id: 29,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine de Comedia",
+            enlace: "https://www.youtube.com/watch?v=_1Z22lgVWUo"
+          },
+          {
+            id: 30,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine Dramático",
+            enlace: "https://www.youtube.com/watch?v=5sOY9h7wbl4"
+          },
+          {id: 31,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine de Animación",
+            enlace: "https://www.youtube.com/watch?v=QhrPT-r3W88"
+          },
+          {id: 32,
+            descripcion: "",
+            autor:"",
+            titulo: "Cine Documental",
+            enlace: "https://www.youtube.com/watch?v=JHkUq8WkWJ0"
+          }
+        ]
+      }
+    ]
+  
+  
